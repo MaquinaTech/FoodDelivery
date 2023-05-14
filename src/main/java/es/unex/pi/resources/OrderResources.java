980e7d6f340a9b9e0483be23ee25fc4c193db7fe
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-05-14 22:03:28 +0200

    make orders

--- src/main/java/es/unex/pi/resources/OrderResources.java
+++ src/main/java/es/unex/pi/resources/OrderResources.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Logger;
+
 import es.unex.pi.dao.OrderDAO;
 import es.unex.pi.dao.JDBCOrderDAOImpl;
 import es.unex.pi.dao.OrderDishesDAO;
@@ -17,19 +19,24 @@ import es.unex.pi.dao.JDBCCategoryDAOImpl;
 import es.unex.pi.dao.JDBCDishDAOImpl;
 import es.unex.pi.dao.JDBCRestaurantCategoriesDAOImpl;
 import es.unex.pi.dao.JDBCRestaurantDAOImpl;
+import es.unex.pi.dao.JDBCTokenDAOImpl;
 import es.unex.pi.dao.RestaurantCategoriesDAO;
 import es.unex.pi.model.Category;
 import es.unex.pi.model.Dish;
 import es.unex.pi.model.Restaurant;
 import es.unex.pi.model.RestaurantCategories;
+import es.unex.pi.model.Token;
 import es.unex.pi.dao.RestaurantDAO;
+import es.unex.pi.dao.TokenDAO;
 import es.unex.pi.model.User;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
@@ -48,6 +55,7 @@ public class OrderResources {
 	ServletContext sc;
 	@Context
 	UriInfo uriInfo;
+	private static final Logger logger = Logger.getLogger(HttpServlet.class.getName());
 	
 	@GET
 	@Path("/{id}")
@@ -57,6 +65,54 @@ public class OrderResources {
 		OrderDAO orderDAO = new JDBCOrderDAOImpl();
 		orderDAO.setConnection(conn);
 		return orderDAO.get(id);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response makeOrder(List<Dish> dishes, @Context HttpServletRequest request) {
+		Connection conn = (Connection) sc.getAttribute("dbConn");
+		logger.info("Hemos llegao");
+		DishDAO dishDAO = new JDBCDishDAOImpl();
+	    dishDAO.setConnection(conn);
+	    int total = 0;		    
+	    for(Dish d: dishes) {
+	    	total += d.getPrice();
+	    }	    
+	    OrderDAO orderDAO = new JDBCOrderDAOImpl();
+	    orderDAO.setConnection(conn);
+	    Order order = new Order();
+	    
+	    String authHeader = request.getHeader("Authorization");
+		String token= authHeader.substring("Bearer ".length()).trim();
+    	TokenDAO tokenDAO = new JDBCTokenDAOImpl();
+	    tokenDAO.setConnection(conn);
+	    List<Token> listTokens = tokenDAO.getAll();
+	    long idU = -1;
+	    for (Token t : listTokens) {
+	        if (t.getValue().equals(token)) {
+	            idU = t.getIdU();
+	            break;
+	        }
+	    }
+	    
+	    order.setIdu(idU);
+	    order.setTotalPrice(total);
+	    orderDAO.add(order);
+	    
+	    order = orderDAO.get(idU, total);
+	    
+	    OrderDishesDAO orderDishesDAO = new JDBCOrderDishesDAOImpl();
+	    orderDishesDAO.setConnection(conn);
+	    for(Dish d: dishes) {
+		    OrderDishes orderDishes = new OrderDishes();
+		    orderDishes.setIdo(order.getId());
+		    orderDishes.setIddi(d.getId());
+		    orderDishesDAO.add(orderDishes);
+	    }
+		return Response.accepted(uriInfo.getAbsolutePathBuilder().build())
+				.contentLocation(uriInfo.getAbsolutePathBuilder().build()).build();
+
 	}
 	
 	/*@POST
